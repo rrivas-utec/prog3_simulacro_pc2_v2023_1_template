98638e84148e4fe4ea92de83e7e3b3ea0538ad38
rivasr <rivasr@gitlab.buildone.co>
2023-06-09 17:21:00 -0500

    Initial commit

---
+++ autograder/tests/catch_test_2_1/test_2_1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rudri on 9/12/2020.
+//
+#include "catch.hpp"
+#include "redirect_io.h"
+#include "P2.h"
+using namespace std;
+
+static void test_2_1() {
+}
+
+TEST_CASE("Question #2.1") {
+    execute_test("test_2_1.in", test_2_1);
+}
---
+++ autograder/tests/catch_test_2_5/test_2_5.cpp
@@ -0,0 +1,14 @@
+//
+// Created by rudri on 9/12/2020.
+//
+#include "catch.hpp"
+#include "redirect_io.h"
+#include "P2.h"
+using namespace std;
+
+static void test_2_5() {
+}
+
+TEST_CASE("Question #2.5") {
+    execute_test("test_2_5.in", test_2_5);
+}
